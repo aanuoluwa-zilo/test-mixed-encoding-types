b2b10b8b42ea7a00881033dc6344a6b5814603e3
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 17:21:09 +0100

    Add encoding test file: src/python/data_processor_003.py

---
+++ src/python/data_processor_003.py
@@ -0,0 +1,22 @@
+class DataProcessor3:
+    def __init__(self, config):
+        self.config = config
+        self.data = []
+    
+    def process_data(self, data):
+        results = []
+        for item in data:
+            if self._validate_item(item):
+                processed = self._transform_item(item)
+                results.append(processed)
+        return results
+    
+    def _validate_item(self, item):
+        return 'id' in item and 'value' in item
+    
+    def _transform_item(self, item):
+        return {
+            'id': item['id'],
+            'value': item['value'] * 3,
+            'processed': True
+        }
